95fa92a9b047621f11cd34c3236b40d043cd85a2
BayramBilal <bilalbayramb22@gmail.com>
2022-07-24 20:06:00 +0300

    emloyee class and microsoft class updated

--- src/constructors/Employee.java
+++ src/constructors/Employee.java
@@ -8,11 +8,11 @@ public class Employee {
     public static String companyName; // static field belongs to Class
 
     static {
-        companyName="Microsoft";
+        companyName="Microsoft"; // this value is same for all the objects of this class
     }
 
     public Employee() {
-        salary = 10000;
+        salary = 10000;  // when I create an Employee object with default constructor, their salary will be 10000 as default.
     }
 
     public Employee(String n, String jobTitle, int ID, double salary) {
@@ -20,5 +20,16 @@ public class Employee {
         this.jobTitle = jobTitle;
         this.ID = ID;
         this.salary = salary;
+    }
+
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                ",company Name=" + companyName+
+                '}';
     }
 }
